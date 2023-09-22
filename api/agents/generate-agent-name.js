22aad41d02a268b016f0ca9b39deb350716598f9
cornerbodega <merhone@gmail.com>
2023-09-22 02:13:42 -0700

    Update generate-agent-name.js

--- api/agents/generate-agent-name.js
+++ api/agents/generate-agent-name.js
@@ -24,6 +24,10 @@ const openai = new OpenAI({
 import "dotenv/config";
 
 export default async function handler(req, res) {
+  if (req.method !== "POST") {
+    res.status(405).json({ error: "Method not allowed" });
+    return;
+  }
   console.log("GENERATE EXPERTISE ENDPOINT");
   console.log("Input:");
   console.log(req.body);
