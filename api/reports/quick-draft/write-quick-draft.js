1ff8edcb7da05e941528c2b02fb941af7006e6d7
cornerbodega <merhone@gmail.com>
2023-10-24 20:25:00 -0700

    fix local vs production server url

--- api/reports/quick-draft/write-quick-draft.js
+++ api/reports/quick-draft/write-quick-draft.js
@@ -106,9 +106,7 @@ export async function writeDraftFunction(req) {
     newAccumulatedContent += part.choices[0]?.delta?.content || "";
     const saveChunkToFirebase = await saveToFirebase(
       `/${
-        process.env.NEXT_PUBLIC_env
-          ? process.env.NEXT_PUBLIC_env
-          : "localAsyncTasks"
+        process.env.localAsyncTasks ? process.env.localAsyncTasks : "asyncTasks"
       }/${process.env.serverUid}/${req.body.userId}/quickDraft/context/draft`,
       `${newAccumulatedContent}…`
     );
@@ -118,9 +116,7 @@ export async function writeDraftFunction(req) {
   newAccumulatedContent += `${" ".repeat(3)}`;
   const saveDraftToFirebase = await saveToFirebase(
     `/${
-      process.env.NEXT_PUBLIC_env
-        ? process.env.NEXT_PUBLIC_env
-        : "localAsyncTasks"
+      process.env.localAsyncTasks ? process.env.localAsyncTasks : "asyncTasks"
     }/${process.env.serverUid}/${req.body.userId}/quickDraft/context/draft`,
     `${newAccumulatedContent}}`
   );
