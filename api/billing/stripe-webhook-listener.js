0423cd0744045af6515f03dc9514917cc52ac905
cornerbodega <merhone@gmail.com>
2025-01-27 19:48:05 -0800

    Update stripe-webhook-listener.js

--- api/billing/stripe-webhook-listener.js
+++ api/billing/stripe-webhook-listener.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
   console.log("STRIPE WEBHOOK ENDPOINT");
 
   // const { userId, type } = req.body;
-
+  console.log(req.body);
   res
     .status(200)
     .json({ status: 200, message: "Stripe Webhook Received Successfully" });
