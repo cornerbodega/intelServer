57ee7199de01f269754a75601ce9bac35cbff188
cornerbodega <merhone@gmail.com>
2025-01-27 17:26:07 -0800

    Update stripe-webhook-listener.js

--- api/billing/stripe-webhook-listener.js
+++ api/billing/stripe-webhook-listener.js
@@ -1,11 +1,11 @@
-import saveToSupabase from "../../utils/saveToSupabase.js";
-import { getSupabase } from "../../utils/supabase.js";
-import Stripe from "stripe";
+// import saveToSupabase from "../../utils/saveToSupabase.js";
+// import { getSupabase } from "../../utils/supabase.js";
+// import Stripe from "stripe";
 // console.log(process.env.STRIPE_KEY);
-const stripe = new Stripe(process.env.PROD_STRIPE_KEY);
+// const stripe = new Stripe(process.env.PROD_STRIPE_KEY);
 // const stripe = new Stripe(process.env.DEV_STRIPE_KEY);
 export default async function handler(req, res) {
-  const supabase = getSupabase();
+  // const supabase = getSupabase();
   console.log("ONE TIME PAYMENT ENDPOINT REACHED");
   console.log(req.body);
   // "whsec_7e18c95695b159ec59e73a837473cb8bce70b7b74f533da68a78791b25895817"
